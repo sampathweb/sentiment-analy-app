e079be55edb80146929f39259fba779fab4f3608
Ramesh Sampath <ramesh@sampathweb.com>
2014-12-29 05:44:02 -0800

    update to settings

--- api/app/settings.py
+++ api/app/settings.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    MAX_CONTENT_LENGTH = 2 * 1024 * 1024
+    MAX_CONTENT_LENGTH = 3 * 1024 * 1024
     #Upload of files
     ALLOWED_EXTENSIONS = set(['txt', 'csv', 'tsv'])
     DATA_DIR = BASE_DIR + '/uploads/'
